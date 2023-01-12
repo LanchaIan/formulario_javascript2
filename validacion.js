2de913fa4f69113bd3ed887ff934650131838bdb
Ian Santana <95245762+LanchaIan@users.noreply.github.com>
2023-01-12 19:12:32 +0000

    Terminado

--- validacion.js
+++ validacion.js
@@ -1,3 +1,5 @@
+let personas = [];
+
 function funcion_reset() {
     if (confirm("¿Está seguro de que quiere reinicializar todos los" +
     " campos del formulario?")) {
@@ -42,8 +44,10 @@ function funcion_reset() {
             info.push("Libros de texto"); 
           }  
         persona.push(info);
-        let camino = document.getElementById("camino").value;
-        persona.push(camino);
+        let camino = document.getElementById("camino");
+        let camino_texto = camino.options[camino.selectedIndex].innerText;
+        persona.push(camino_texto);
+        personas.push(persona);
         return persona;
     }
     
@@ -51,12 +55,74 @@ function funcion_reset() {
         let persona = conseguir_datos();
         return persona[0] + " " + persona[1] + " Correo electrínico: " + persona[3];
     }
+
+    function conseguir_datos_completos(){
+        let num_persona = get_persona();
+        let persona = personas[num_persona];
+        return "Apellidos: " + persona[0] + "\nNombre: " + persona[1] + "\nsexo: " + persona[2] + "\nCorreo electrínico: " + persona[3] + "\nLlegó hasta aquí " + persona[5] + "\nDesea saber más información sobre: " + persona[4];
+    }
+
+    function calcular_personas(){
+        let total = personas.length;
+        return total;
+    }
+
+    function comprobar(){
+        let completo = true;
+        if (document.getElementById("apellidos").value == ""){
+            completo = false;
+            alert("Debes rellenar el apartado 'Apellidos'");
+        }
+        if (document.getElementById("nombre").value == ""){
+            completo = false;
+            alert("Debes rellenar el apartado 'Nombre'");
+        }
+        let sexo = conseguir_input_radio();
+        if (sexo === undefined){
+            completo = false;
+            alert("Debes rellenar el apartado 'Sexo'");
+        }
+        if (document.getElementById("correo").value == ""){
+            completo = false;
+            alert("Debes rellenar el apartado 'Correo'");
+        }
+        
+        return completo;
+    }
     
     function añadir(){
+        let comprobacion = comprobar();
+        if (comprobacion === false){
+            return;
+        }
         let persona = document.createElement('option');
         let datos = conseguir_datos_resumidos();
-        persona.value = datos;
+        let num_personas = calcular_personas();
+        persona.value = num_personas;
         persona.text = datos;
         let $select = document.querySelector("#personas");
         $select.appendChild(persona);
+    }
+
+    function get_persona(){
+        let persona = document.getElementById("personas");
+        let numero_persona = persona.value - 1;
+        return numero_persona;
+}
+    function ver_datos(){
+        if (document.getElementById("personas").value == ""){
+            alert("Debes seleccionar una persona para ver sus datos");
+        }
+        let persona = conseguir_datos_completos();
+        alert(persona);
+    }
+
+    function borrar_datos(){
+        if (document.getElementById("personas").value == ""){
+            alert("Debes seleccionar una persona para borrar sus datos");
+        }
+        let persona = document.getElementById("personas");
+        let num_persona = get_persona();
+        persona.remove(persona.selectedIndex);
+        personas.splice(num_persona, num_persona + 1);
     }
